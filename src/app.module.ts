cdd59d6e02d71015942a569b949919370b9d3e15
Vinicius Alberti <paralvlup.cal@gmail.com>
2022-07-30 12:47:20 -0300

    feat: add admin middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { RentsModule } from './rents/rents.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { LoginMiddleware } from './middlewares/login.middleware';
+import { AdminMiddleware } from './middlewares/admin.middleware';
 
 @Module({
   imports: [VehiclesModule, RentsModule, UsersModule, AuthModule],
@@ -11,5 +17,8 @@ import { LoginMiddleware } from './middlewares/login.middleware';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoginMiddleware).forRoutes(VehiclesModule, RentsModule);
+    consumer
+      .apply(AdminMiddleware)
+      .forRoutes({ path: 'vehicles', method: RequestMethod.POST });
   }
 }
---
+++ src/middlewares/admin.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { User } from 'src/users/interfaces/User';
+
+@Injectable()
+export class AdminMiddleware implements NestMiddleware {
+  private users: User[] = [];
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req;
+
+    const user = this.users.find((user) => user.id === userId);
+
+    if (user.isAdmin) {
+      return next();
+    }
+
+    return res.status(401).json({
+      error: 'Unauthorized',
+    });
+  }
+}
